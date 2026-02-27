7622511727c031732e3e730ead1141872f5d998a
TMM1003 <102240828+TMM1003@users.noreply.github.com>
2026-02-26 19:57:51 -0500

    fixing merge conflicts

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -9,9 +9,14 @@ public class DmvSimulator {
         for (int offset = 1; offset < maxNumber; offset++) {
             int calledNumber = ((userNumber - 1 + offset) % maxNumber) + 1;
             System.out.println("Now serving number " + calledNumber + ".");
-        }
+            System.out.println("Now serving number " + userNumber + ".");
 
-        System.out.println("Now serving number " + userNumber + ".");
-        System.out.println("You don't have the required paperwork you literal nasty little piece of human garbage scum MWAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA.");
+            if (Math.random() < 0.99) {
+                System.out.println("You don't have the required paperwork you literal nasty little piece of human garbage scum MWAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA.");
+            } else {
+                System.out.println("You have all of the required paperwork and you are all set.");
+                
+            }
+        }
     }
 }
